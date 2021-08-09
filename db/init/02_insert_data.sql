e34ecb795d158daaec4c57042e1ae99b8ea519be
nakajima_git01 <saddlemanxx@yahoo.co.jp>
2021-08-09 21:14:04 +0900

    first commit

---
+++ db/init/02_insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO book VALUES(DEFAULT,'プログラミング言語C',current_timestamp),
+INSERT INTO book VALUES(DEFAULT,'やさしいコンピューター科学',current_timestamp),
+INSERT INTO book VALUES(DEFAULT,'ゲーデル、エッシャー、バッハ―あるいは不思議の環',current_timestamp),
+INSERT INTO book VALUES(DEFAULT,'TeXブックコンピュータによる組版システム',current_timestamp),
+INSERT INTO book VALUES(DEFAULT,'人月の神話狼人間を撃つ銀の弾はない',current_timestamp),
---
+++ db/init/03_setting.sql
@@ -0,0 +1,1 @@
+settimezoneto'Asia/Tokyo';
